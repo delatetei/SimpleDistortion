33846faaac857d8d550e2a88b2da2e26c7419126
delatetei <ambulamce@gmail.com>
2017-09-12 01:00:06 +0900

    プリセット保存／読込処理の実装

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -181,15 +181,16 @@ AudioProcessorEditor* SimpleDistortionAudioProcessor::createEditor()
 //==============================================================================
 void SimpleDistortionAudioProcessor::getStateInformation (MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    ScopedPointer<XmlElement> xml(parameters.state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void SimpleDistortionAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    ScopedPointer<XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+    if (xmlState != nullptr)
+        if (xmlState->hasTagName(parameters.state.getType()))
+            parameters.state = ValueTree::fromXml(*xmlState);
 }
 
 //==============================================================================
